c3a281d310d1dd5561e2fdb1e63fefa30858fab8
Ernist Akunzhanov <akunzhanovernist@gmail.com>
2022-06-23 09:38:36 +0300

    worked with details page

--- lib/pages/details_page.dart
+++ lib/pages/details_page.dart
@@ -3,6 +3,7 @@ import 'package:cake_app/constants/text_style.dart';
 import 'package:cake_app/data/items.dart';
 import 'package:cake_app/widget/rating_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:readmore/readmore.dart';
 
 class DetailPage extends StatefulWidget {
   final Cakes cake;
@@ -16,6 +17,7 @@ class DetailPage extends StatefulWidget {
 }
 
 class _DetailPageState extends State<DetailPage> {
+  int qty = 1;
   @override
   Widget build(BuildContext context) {
     final height = MediaQuery.of(context).size.height;
@@ -54,15 +56,72 @@ class _DetailPageState extends State<DetailPage> {
                   const SizedBox(height: 10.0),
                   Row(
                     children: [
-                      StarRating(
-                        rating: widget.cake.rating,
+                      StarRating(rating: widget.cake.rating),
+                      Text('${widget.cake.rating}'),
+                      const SizedBox(width: 120.0),
+                      miniButton(
+                        onTap: () {
+                          if (qty > 1) setState(() => qty--);
+                        },
+                        icon: Icons.remove,
+                        iconColor: mainColor,
+                        bgColor: pink01,
+                      ),
+                      Text(
+                        qty > 9 ? '$qty' : '0$qty',
+                        style: txtItemCard.copyWith(
+                          color: mainColor,
+                        ),
+                      ),
+                      miniButton(
+                        onTap: () {
+                          setState(() => qty++);
+                        },
+                        icon: Icons.add,
+                        bgColor: mainColor,
                       ),
                     ],
+                  ),
+                  const SizedBox(height: 20.0),
+                  Container(
+                    width: 360.0,
+                    height: 200,
+                    child: ListView(
+                      padding: EdgeInsets.only(bottom: 50.0),
+                      physics: BouncingScrollPhysics(),
+                      children: [
+                        ReadMoreText(
+                          widget.cake.destription,
+                          trimLines: 7,
+                          trimMode: TrimMode.Line,
+                        ),
+                      ],
+                    ),
                   ),
                 ],
               ),
             ),
           ],
+        ),
+      ),
+    );
+  }
+
+  Widget miniButton({
+    required Function() onTap,
+    required Color bgColor,
+    required IconData icon,
+    Color? iconColor,
+  }) {
+    return GestureDetector(
+      onTap: onTap,
+      child: CircleAvatar(
+        backgroundColor: bgColor,
+        radius: 12.0,
+        child: Icon(
+          icon,
+          color: iconColor != null ? iconColor : white,
+          size: 15,
         ),
       ),
     );
